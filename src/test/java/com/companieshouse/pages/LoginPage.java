5b32784bb4d4ea51dd290a0fedf0b9c3380d66d0
Ayatsalah <eng.hard@yahoo.com>
2025-07-12 02:05:54 +0100

    Locate the login page web elements

---
+++ src/test/java/com/companieshouse/pages/LoginPage.java
@@ -0,0 +1,32 @@
+package com.companieshouse.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+
+    @FindBy(xpath = "//input[@id='username']")
+    public WebElement usernameInput;
+
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement passwordInput;
+
+    @FindBy(xpath = "//button[@id='doLogin']")
+    public WebElement loginBtn;
+
+    @FindBy(xpath = "//div[@role='alert']")
+    public WebElement alertMessage;
+
+    @FindBy(xpath = "//button[.='Logout']")
+    public WebElement logoutBtn;
+
+
+
+    public void login(String username, String password){
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        loginBtn.click();
+
+
+    }
+}
